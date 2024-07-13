7a8f3a5f8b2867ae0e20b6b8fe8dfb7acc7e8e5f
lepi ai <50534209+vxinhao@users.noreply.github.com>
2024-07-13 23:54:35 +0800

    Add files via upload
    独立节点，可按需单独安装，如果clone本项目则自动安装所有节点。

---
+++ allTools/color2rgb.py
@@ -0,0 +1,52 @@
+class AnyType(str):
+    """A special type that can be connected to any other types. Credit to pythongosssss"""
+
+    def __ne__(self, __value: object) -> bool:
+        return False
+
+any_type = AnyType("*")
+
+# ---------------------------------------------------------------
+
+
+class color2RGB:
+    # @乐皮ai 2024-05-09
+
+    @classmethod
+    def INPUT_TYPES(s):  
+    
+        return {
+            "required": {
+                "colorDEC": ("STRING", {"forceInput": True}),
+            },        
+            }
+
+    RETURN_TYPES = (any_type, any_type, any_type,  )
+    RETURN_NAMES = ("R", "G", "B",  )    
+    FUNCTION = "split"
+
+    CATEGORY = "MiniTools"
+
+    def split(self, colorDEC):
+
+
+        tulpeSn = list(colorDEC)
+        strings = [part for part in tulpeSn[:3]]
+        R, G, B, = strings + [""] * (3 - len(strings))          
+
+        return (R, G, B, )
+
+
+
+# 菜单名
+NODE_CLASS_MAPPINGS = {
+    "LP-color2RGB-m"    : color2RGB
+}
+ 
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LP-color2RGB-m"    : "Color to RGB 👻"
+}
+
+
+
